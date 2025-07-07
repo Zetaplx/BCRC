35a3ad44478778eedc934f69d2bdbe06618815c2
Zetaplx <25778189+Zetaplx@users.noreply.github.com>
2025-07-07 11:18:32 -0500

    Initial Program Setup

---
+++ ConsoleTest/Main.cs
@@ -0,0 +1,9 @@
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("This is a test of the console application.");
+    }
+}
