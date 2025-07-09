5cc5d579766276d82e0e2239778fb432085042ca
Zetaplx <kylgrg.public@gmail.com>
2025-07-09 14:57:12 -0500

    Refactored RecipeComponent to use unified OutputData structure.

--- ConsoleTest/BCRC Components/RecipeComponents.cs
+++ ConsoleTest/BCRC Components/RecipeComponents.cs
@@ -5,54 +5,23 @@ using YamlDotNet.Serialization;
 public class RecipeComponent : Component
 {
     public List<InputData> Inputs { get; set; } = new();
-    public List<OutputStatic> StaticOutputs { get; set; } = new();
-    public List<OutputRange> RangeOutputs { get; set; } = new();
-    public List<OutputBuckets> BucketOutputs { get; set; } = new();
-
+    public List<OutputData> Outputs { get; set; } = new();
+    
     public class InputData
     {
         public string ItemId { get; set; } = "invalid";
         public int Quanity { get; set; } = 1;
     }
 
-    /*
-        One possible reimagining of this system
-        can be gleamed from the BitCraftDex
-        representation of "Outputs"
-
-        Basically, there are only buckets. Everything
-        is a bucket. Static outputs? Buckets with only
-        1 bucket. Range outputs? One bucket for each item
-        quantity! Bucket outputs are the same, obviously.
-
-        This dramatically simplifies the output architecture
-        of the recipe system. No more 3 different types of
-        outputs with different classes. Just the one unified system.
-
-    */
-
-    public class OutputStatic
+    public class OutputData
     {
-        public string ItemId { get; set; } = "invalid";
-        public int Quantity { get; set; } = 1;
-    }
-
-    public class OutputRange
-    {
-        public string ItemId { get; set; } = "invalid";
-        public int Min { get; set; } = 0;
-        public int Max { get; set; } = 1;
-    }
-
-    public class OutputBuckets
-    {
-        public List<Bucket> Buckets { get; set; } = new();
-
-        public class Bucket
+        public int Weight { get; set; } = 1;
+        public List<OutputItem> Items { get; set; } = new();
+        
+        class OutputItem 
         {
-            public int Weight { get; set; } = 1;
-            public List<OutputStatic> StaticOutputs { get; set; } = new();
-            public List<OutputRange> RangeOutputs { get; set; } = new();
+            public string ItemId { get; set; } = "invalid";
+            public int Quantity { get; set; } = 1;
         }
     }
 }
